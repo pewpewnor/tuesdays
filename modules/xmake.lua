1bc9481bf0883494dffda21e9a1db797147aefa7
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-12-17 14:53:09 +0800

    feat: add lto rule

--- modules/xmake.lua
+++ modules/xmake.lua
@@ -1,14 +1,17 @@
 target("tuesdays-modules-layout")
+    add_rules("build-target")
     add_files("layout/**.cpp")
     add_deps("tuesdays-core")
     add_packages("imgui-sfml", "spdlog", { public = true })
 
 target("tuesdays-modules-iws")
+    add_rules("build-target")
     add_files("iws/**.cpp")
     add_includedirs(".")
     add_deps("tuesdays-core")
     add_packages("imgui-sfml", "spdlog", { public = true })
 
 target("tuesdays-modules")
+    add_rules("build-target")
     add_includedirs(".", { public = true })
     add_deps("tuesdays-modules-iws", "tuesdays-modules-layout")
--- xmake.lua
+++ xmake.lua
@@ -23,11 +23,19 @@ add_requires("nlohmann_json")
 add_requires("spdlog")
 add_requireconfs("*", { configs = { shared = not is_mode("release") } })
 
+rule("build-target")
+    if is_mode("release") then
+        on_load(function(target)
+            target:set("policy", "build.optimization.lto", true)
+        end)
+    end
+
 includes("core")
 includes("modules")
 
 target("tuesdays-app")
     set_kind("binary")
+    add_rules("build-target")
     add_files("app/**.cpp")
     add_deps("tuesdays-modules", "tuesdays-core")
     add_packages("spdlog")
